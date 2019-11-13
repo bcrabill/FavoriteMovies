676e366f38941d11551af08ad7f9052812e83194
bcrabill <51893306+bcrabill@users.noreply.github.com>
2019-11-13 15:54:14 -0600

    API call and forms corrected

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -26,33 +26,63 @@ namespace FavoriteMovies.Controllers
         public IActionResult Index()
         {
             //List<Movie> allFavorites = dbContext.Movies.Include( u => u.FavoritedBy).ToList();
-
+            Console.WriteLine("**************************");
             return View("Index");
         }
 
        
-        [HttpGet("movie/{Title}")]
-        [Route("movie/{Title}")]
+       
         
-        public async Task<ActionResult> GetTitle(string Title)
+        public async Task<Movie> APIProcessor(string newTitle)
         {
-            var newTitle = Title.Replace(' ','+');
-            Console.WriteLine("the new title is "+ newTitle);
-            
             using (var client = new HttpClient())
             {
                 string url = "http://www.omdbapi.com?";
                 string apiKey = "apikey=8bc90e0b&";
                 string titleInput = "t="+newTitle;
                 client.BaseAddress = new Uri(url+apiKey+titleInput);
+                Console.WriteLine("the url entered is"+client.BaseAddress);
                 var task = await client.GetAsync(client.BaseAddress);
                 var jsonString = await task.Content.ReadAsStringAsync();
                 Console.WriteLine(jsonString);
                 var movieResult = JsonConvert.DeserializeObject<Movie>(jsonString);
-                return View("MovieDetails", movieResult);
+                return movieResult;
             }           
         }
-
+        /*public IActionResult AddMovie(Movie newMovie)
+        {
+            User userInDb = dbContext.Users.FirstOrDefault( u => u.UserId == HttpContext.Session.GetInt32("UserId"));
+            if(HttpContext.Session.GetInt32("UserId") == null)
+            {
+                return RedirectToAction("Logout", "Home");
+            }
+            else
+            {
+                if(ModelState.IsValid)
+                {
+                    newMovie.FavoritedBy = userInDb;
+                    dbContext.Movies.Add(newMovie);
+                    dbContext.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("ShowMovie");
+                }
+            }
+        }
+        */
+        [HttpPost]
+        [Route("movie")]
+        public IActionResult TitleSearch(string Title)
+        {
+            Console.WriteLine("##########################################");
+            var newTitle = Title.Replace(' ','+');
+            Console.WriteLine("the new title is "+ newTitle);
+            Movie movieTitle = APIProcessor(newTitle).Result;
+            
+            return View("MovieDetails", movieTitle);
+        }
        
 
     }
